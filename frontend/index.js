5e88e30ab8381dd345c70623746787a45e4cec85
nwhayes7 <nwhayes7@gmail.com>
2021-11-06 22:25:13 -0400

    Linear regression

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,6 @@ var obj_csv = {
 };
  
 function readImage(input) {
-    //csvData = []
     if (input.files && input.files[0]) {
         let reader = new FileReader();
         reader.readAsBinaryString(input.files[0]);
@@ -13,6 +12,7 @@ function readImage(input) {
             obj_csv.dataFile = e.target.result
             csvData = parseData(obj_csv.dataFile)
             csvData.shift()
+            csvData.pop()
         }
     }
 }
@@ -29,3 +29,26 @@ function parseData(data){
 function generateData(){
     console.log(csvData)
 }
+
+function getMaxSlopesAndIds(){ //numberOfIds
+    var slopes = {} //create dictionary to hold slopes with ids as key
+    var maxIds = [] //create list to house the id's with highest slopes (greatest increase in demand)
+    var lrModels = []
+    for(let i = 0; i < csvData.length; i++) {
+        let lrVals = [[1, parseInt(csvData[i][5])], [2, parseInt(csvData[i][6])], [3, parseInt(csvData[i][7])], [4, parseInt(csvData[i][8])], [5, parseInt(csvData[i][9])], [6, parseInt(csvData[i][10])]] //store pair Values in an Array
+        lrModels[i] = regression.linear(lrVals);
+        
+        console.log(lrVals)
+        console.log(lrModels[i].equation[0])
+        //slopes[csvData[i][0]] = lr.equation[0]
+    }
+
+}
+
+// const gradient = result.equation[0];
+// const yIntercept = result.equation[1];
+
+//Calculate slope for every row (Month as x, new value as y)
+//get max slope (or top 20-30 or something), these are doctors trending higher
+//show individual data points month to month
+//use r2 to check reliability of fit (low r2 may show outliers, data not as it seems)
